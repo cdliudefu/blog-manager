891335a08036aeda54adf358b1cda1e457f020b6
cdliudefu <cdliudefu@126.com>
2020-04-26 21:46:18 +0800

    update files

---
+++ README.md
@@ -0,0 +1,2 @@
+### 1、模板 插槽
+bcrypt加密  npm i bcrypt  npm i -g node-gyp  npm install --global--production windows-build-tools
--- route/admin.js
+++ route/admin.js
@@ -1,36 +1,37 @@
 const express = require('express')
-//实例路由对象
+    //实例路由对象
 const router = express.Router()
 
 router.get('/login', (req, res) => {
-  res.render('admin/login', { msg: 'test' })
+    res.render('admin/login', { msg: 'test' })
 })
 
 //提交登陆
 router.post('/index', (req, res) => {
-  console.log('res==', req.body)
-  const { email, password } = req.body
-  res.render('admin/common/layout', { user: email })
+    console.log('res==', req.app)
+    const { email, password } = req.body
+
+    res.redirect('/admin/dashbord')
 })
 
 //dashbord
 router.get('/dashbord', (req, res) => {
-  res.render('admin/dashbord', { user: email })
+    res.render('admin/dashbord', { user: 'lisi' })
 })
 
 //用户
 router.get('/user', (req, res) => {
-  res.render('admin/user', { user: email })
+    res.render('admin/user', { user: 'user' })
 })
 
 //文章
 router.get('/article', (req, res) => {
-  res.render('admin/article', { user: email })
+    res.render('admin/article', { user: 'article' })
 })
 
 //设置
 router.get('/setting', (req, res) => {
-  res.render('admin/setting', { user: email })
+    res.render('admin/setting', { user: 'setting' })
 })
 
 module.exports = router
